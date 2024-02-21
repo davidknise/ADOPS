fb9ad99f0d8754ab436452d4c8544c1e2eef2423
David Knise <daknis@microsoft.com>
2024-02-21 07:51:04 -0800

    Update New-AdoGitRepository

--- Repositories/New-AdoGitRepository.psm1
+++ Repositories/New-AdoGitRepository.psm1
@@ -11,46 +11,26 @@ Function New-AdoGitRepository
         [String] $OutFile
     )
 
-    if ($RepoName)
-    {
-        $repos = Get-AdoRepositories `
-            -Organization $Organization `
-            -Project $Project
+    $uri = 'https://dev.azure.com/{organization}/{project}/_apis/git/repositories'
+    $uri += 'api-version=6.0'
 
-        foreach ($repo in $repos.value)
-        {
-            if ($repo.name -ieq $RepoName)
-            {
-                Write-Output $repo
-                break
+    $response = Invoke-AdoRestMethod `
+        -Uri $uri `
+        -Method 'POST' `
+        -ReplaceKeys @{
+            organization = $Organization
+            project = $Project
+            repositoryId = $RepositoryId
+        } `
+        -BodyObject @{
+            name = $RepoName
+            project = @{
+                id = $ProjectId
             }
-        }
+        } `
+        -OutFile $OutFile
 
-        # Not Found
-    }
-    else
-    {
-        $uri = 'https://dev.azure.com/{organization}/{project}/_apis/git/repositories'
-        $uri += 'api-version=6.0'
-
-        $response = Invoke-AdoRestMethod `
-            -Uri $uri `
-            -Method 'POST' `
-            -ReplaceKeys @{
-                organization = $Organization
-                project = $Project
-                repositoryId = $RepositoryId
-            } `
-            -BodyObject @{
-                name = $RepoName
-                project = @{
-                    id = $ProjectId
-                }
-            } `
-            -OutFile $OutFile
-
-        Write-Output $response
-    }
+    Write-Output $response
 }
 
 Export-ModuleMember -Function @(
